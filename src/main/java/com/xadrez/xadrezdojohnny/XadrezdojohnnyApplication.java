d547d0f52433d0f7c9f7632cd81cbe99af80e228
JohnnyGili <110643131+JohnnyGili@users.noreply.github.com>
2024-06-17 23:23:57 -0300

    TrabalhoEleniltonXadrez

---
+++ src/main/java/com/xadrez/xadrezdojohnny/XadrezdojohnnyApplication.java
@@ -0,0 +1,13 @@
+package com.xadrez.xadrezdojohnny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XadrezdojohnnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XadrezdojohnnyApplication.class, args);		
+	}
+
+}
---
+++ src/test/java/com/xadrez/xadrezdojohnny/XadrezdojohnnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xadrez.xadrezdojohnny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XadrezdojohnnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
